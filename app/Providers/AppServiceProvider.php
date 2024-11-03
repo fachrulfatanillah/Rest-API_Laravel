771471294fae855976a0b2a77d89f6d31025e6df
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2024-11-03 17:31:35 +0700

    Create Post Users

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->mapApiRoutes();
+    }
+
+    protected function mapApiRoutes()
+    {
+        Route::middleware('api')
+            ->prefix('post')
+            ->group(base_path('routes/api.php'));
     }
 }
